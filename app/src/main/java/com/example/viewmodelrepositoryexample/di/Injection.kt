223fa96852a598243df7a5efa8bc9fb6646784cf
paulinagh <paulinanel50@gmail.com>
2023-10-23 23:24:38 +0700

    Menambahkan komentar kode pada file Injection

--- app/src/main/java/com/example/viewmodelrepositoryexample/di/Injection.kt
+++ app/src/main/java/com/example/viewmodelrepositoryexample/di/Injection.kt
@@ -5,6 +5,11 @@ import com.example.viewmodelrepositoryexample.data.remote.ApiConfig
 import com.example.viewmodelrepositoryexample.data.repository.CatRepository
 
 object Injection {
+    /**
+     * Fungsi ini akan dipanggil oleh kelas extends ViewModel yang membutuhkan parameter dengan tipe data CatRepository di dalam ViewModelFactory
+     *
+     * Injeksi seperti ini dapat digunakan kembali oleh kelas-kelas yang membutuhkan parameter dengan tipe data CatRepository
+     */
     fun provideCatRepository(context: Context): CatRepository {
         val apiService = ApiConfig.getApiService()
         return CatRepository(apiService)
