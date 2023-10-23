fc551fdeeaaee835ef69b6d1fc6ee3bc517070d5
paulinagh <paulinanel50@gmail.com>
2023-10-23 12:40:47 +0700

    Menambahkan komentar kode pada file CatRepository

--- app/src/main/java/com/example/viewmodelrepositoryexample/data/repository/CatRepository.kt
+++ app/src/main/java/com/example/viewmodelrepositoryexample/data/repository/CatRepository.kt
@@ -9,14 +9,21 @@ import com.example.viewmodelrepositoryexample.data.response.CatItemResponse
 class CatRepository(
     private val apiService: ApiService
 ) {
-    //
+    /**
+     * Fungsi ini akan dipanggil oleh fungsi getRandomCatList() di halaman CatViewModel
+     *
+     * Tipe data: LiveData<ResultState<List<CatItemResponse>>>
+     */
     fun getRandomCatList(): LiveData<ResultState<List<CatItemResponse>>> = liveData {
+        // Loading
         emit(ResultState.Loading)
         try {
             val response = apiService.getRandomCatList()
-            val articles = response
-            emit(ResultState.Success(articles))
+
+            // Mengirimkan data dari API ketika sukses
+            emit(ResultState.Success(response))
         } catch (e: Exception) {
+            // Gagal
             emit(ResultState.Error(e.message.toString()))
         }
     }
